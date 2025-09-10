4dea5b0df4ccb4fa5c7d2382ef344ec8c81ae127
j-huang9 <jennaaahuang09@gmail.com>
2025-09-10 13:27:46 +0000

    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,7 +189,7 @@ elif st.session_state.page == 3:
             col3, col4 = st.columns(2)
             with col3:
                 st.markdown("""
-                **How likely would you be to engage with this ad?**  
+                **How likely would you be to engage with this ad? Engagement could include clicking, sharing, saving, taking a photo, or talking about it with others.**  
                 1 = Very unlikely → “I would definitely not engage with this.”  
                 2 = Unlikely → “I probably wouldn’t engage with this.”  
                 3 = Neutral / Maybe → “I might or might not engage with this.”  
